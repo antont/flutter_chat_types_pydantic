84992fcaafb91307f8e8bfa41558c62b3d89e2e1
Toni Alatalo <toni@playsign.net>
2023-11-14 16:33:20 +0200

    convert to pydantic. the lib to do this is buggy WIP still

--- lib/src/messages/file_message.g.dart
+++ lib/src/messages/file_message.g.dart
@@ -1,76 +1,25 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
+// #GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'file_message.dart';
 
 // **************************************************************************
-// JsonSerializableGenerator
+// PydanticSerializableGenerator
 // **************************************************************************
 
-FileMessage _$FileMessageFromJson(Map<String, dynamic> json) => FileMessage(
-      author: User.fromJson(json['author'] as Map<String, dynamic>),
-      createdAt: json['createdAt'] as int?,
-      id: json['id'] as String,
-      isLoading: json['isLoading'] as bool?,
-      metadata: json['metadata'] as Map<String, dynamic>?,
-      mimeType: json['mimeType'] as String?,
-      name: json['name'] as String,
-      remoteId: json['remoteId'] as String?,
-      repliedMessage: json['repliedMessage'] == null
-          ? null
-          : Message.fromJson(json['repliedMessage'] as Map<String, dynamic>),
-      roomId: json['roomId'] as String?,
-      showStatus: json['showStatus'] as bool?,
-      size: json['size'] as num,
-      status: $enumDecodeNullable(_$StatusEnumMap, json['status']),
-      type: $enumDecodeNullable(_$MessageTypeEnumMap, json['type']),
-      updatedAt: json['updatedAt'] as int?,
-      uri: json['uri'] as String,
-    );
-
-Map<String, dynamic> _$FileMessageToJson(FileMessage instance) {
-  final val = <String, dynamic>{
-    'author': instance.author.toJson(),
-  };
-
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
-    }
-  }
-
-  writeNotNull('createdAt', instance.createdAt);
-  val['id'] = instance.id;
-  writeNotNull('metadata', instance.metadata);
-  writeNotNull('remoteId', instance.remoteId);
-  writeNotNull('repliedMessage', instance.repliedMessage?.toJson());
-  writeNotNull('roomId', instance.roomId);
-  writeNotNull('showStatus', instance.showStatus);
-  writeNotNull('status', _$StatusEnumMap[instance.status]);
-  val['type'] = _$MessageTypeEnumMap[instance.type]!;
-  writeNotNull('updatedAt', instance.updatedAt);
-  writeNotNull('isLoading', instance.isLoading);
-  writeNotNull('mimeType', instance.mimeType);
-  val['name'] = instance.name;
-  val['size'] = instance.size;
-  val['uri'] = instance.uri;
-  return val;
-}
-
-const _$StatusEnumMap = {
-  Status.delivered: 'delivered',
-  Status.error: 'error',
-  Status.seen: 'seen',
-  Status.sending: 'sending',
-  Status.sent: 'sent',
-};
-
-const _$MessageTypeEnumMap = {
-  MessageType.audio: 'audio',
-  MessageType.custom: 'custom',
-  MessageType.file: 'file',
-  MessageType.image: 'image',
-  MessageType.system: 'system',
-  MessageType.text: 'text',
-  MessageType.unsupported: 'unsupported',
-  MessageType.video: 'video',
-};
+class FileMessage(BaseModel) :  
+      author: author: Any,
+      createdAt: createdAt: int,
+      id: id: str,
+      isLoading: isLoading: bool,
+      metadata: metadata: dict,
+      mimeType: mimeType: str,
+      name: name: str,
+      remoteId: remoteId: str,
+      repliedMessage: repliedMessage: Any,
+      roomId: roomId: str,
+      showStatus: showStatus: bool,
+      size: size: Any,
+      status: status: Any,
+      type: type: Any,
+      updatedAt: updatedAt: int,
+      uri: uri: str,
--- lib/src/messages/partial_image.g.dart
+++ lib/src/messages/partial_image.g.dart
@@ -1,38 +1,16 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
+// #GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'partial_image.dart';
 
 // **************************************************************************
-// JsonSerializableGenerator
+// PydanticSerializableGenerator
 // **************************************************************************
 
-PartialImage _$PartialImageFromJson(Map<String, dynamic> json) => PartialImage(
-      height: (json['height'] as num?)?.toDouble(),
-      metadata: json['metadata'] as Map<String, dynamic>?,
-      name: json['name'] as String,
-      repliedMessage: json['repliedMessage'] == null
-          ? null
-          : Message.fromJson(json['repliedMessage'] as Map<String, dynamic>),
-      size: json['size'] as num,
-      uri: json['uri'] as String,
-      width: (json['width'] as num?)?.toDouble(),
-    );
-
-Map<String, dynamic> _$PartialImageToJson(PartialImage instance) {
-  final val = <String, dynamic>{};
-
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
-    }
-  }
-
-  writeNotNull('height', instance.height);
-  writeNotNull('metadata', instance.metadata);
-  val['name'] = instance.name;
-  writeNotNull('repliedMessage', instance.repliedMessage?.toJson());
-  val['size'] = instance.size;
-  val['uri'] = instance.uri;
-  writeNotNull('width', instance.width);
-  return val;
-}
+class PartialImage(BaseModel) :  
+      height: height: float,
+      metadata: metadata: dict,
+      name: name: str,
+      repliedMessage: repliedMessage: Any,
+      size: size: Any,
+      uri: uri: str,
+      width: width: float,
