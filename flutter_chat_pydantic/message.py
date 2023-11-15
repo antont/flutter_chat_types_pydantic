a387781d883e404104d7a4591988385080b0abbe
Toni Alatalo <toni@playsign.net>
2023-11-15 12:01:31 +0200

    Message does not just @json_serializable so converted it to pydantic manually

---
+++ flutter_chat_pydantic/message.py
@@ -0,0 +1,36 @@
+from enum import StrEnum, auto
+from typing import Optional
+
+from pydantic import BaseModel
+
+from user import User
+from message import Message
+
+class MessageType(StrEnum):
+    audio = auto()
+    custom = auto()
+    file = auto()
+    image = auto()
+    system = auto()
+    text = auto()
+    unsupported = auto()
+    video = auto()
+
+class Status(StrEnum):
+    delivered = auto()
+    error = auto()
+    seen = auto()
+    sending = auto()
+    sent = auto()
+
+class Message(BaseModel):
+    author: User
+    createdAt: Optional[int] = None
+    id: str
+    metadata: Optional[dict] = None
+    remoteId: Optional[str] = None
+    repliedMessage: Optional[Message] = None
+    roomId: Optional[str] = None
+    showStatus: Optional[bool] = None
+    status: Optional[Status] = None
+    updatedAt: Optional[int] = None
